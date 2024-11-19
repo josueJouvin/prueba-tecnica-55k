35cf39b12eed63b6cbbd3227ebc6a4f5a6457db0
Josue Garces Jouvin <josuejouvin27@gmail.com>
2024-11-19 17:11:26 -0500

    feat: tabla de datos con filtrado y coloreado

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import { User } from "./types";
+
+function App() {
+  const [users, setUser] = useState<User[]>([])
+  const [color, setColor] = useState(false)
+  const [sortByCountry, setSortByCountry] = useState(false)
+
+  useEffect(( ) => {
+    fetch(`https://randomuser.me/api/?results=100`)
+    .then(response => response.json())
+    .then(data => {
+      setUser(data.results)
+    })
+  },[])
+
+  const toggleColor = () => {
+    setColor(!color)
+  }
+
+  const toggleOrderCountry = () =>{
+    setSortByCountry(!sortByCountry)
+  }
+
+  const sortUsers = sortByCountry ? [...users].sort((a, b) => {return a.location.country.localeCompare(b.location.country)}): users
+
+  return (
+    <>
+      <h1>Prueba tecnica ssr</h1>
+      <div className="filters">
+        <button onClick={toggleColor}>Colorear Filas</button>
+        <button onClick={toggleOrderCountry}>{sortByCountry ? "No ordenar por pais" : "Ordenar por pais"}</button>
+
+      </div>
+
+      <table className={`table ${color ? "colorRows" : ""}`}>
+        <thead>
+          <tr>
+            <th>Foto</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Pais</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            sortUsers.map(user => (
+              <tr key={user.login.uuid}>
+                <td style={{width: "auto"}}>
+                  <img src={user.picture.thumbnail}  />
+                </td>
+                <td>{user.name.first}</td>
+                <td>{user.name.last}</td>
+                <td>{user.location.country}</td>
+                <td>
+                  <button>Eliminar</button>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default App;
